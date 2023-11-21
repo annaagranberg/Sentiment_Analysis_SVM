2cf8641c3f090f17990022acf22d95c3a7a9f550
hamreliusfilip <91670593+hamreliusfilip@users.noreply.github.com>
2023-11-21 15:58:49 +0100

    Plotting dataset with PCA

--- SVM.py
+++ SVM.py
@@ -14,5 +14,40 @@ train_y_1 = loaded_data['train_y_1']
 test_x_1 = loaded_data['test_x_1']
 test_y_1 = loaded_data['test_y_1']
 
+# Check the dimensions of the data
+print(len(train_x_0), len(train_y_0))
+print(test_x_0.shape, test_y_0.shape)
+
+# Plotting with PCA 
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition import PCA
+
+# Assuming your x and y variables are named x_data and y_data
+x_data = train_x_0
+y_data = train_y_0
+
+# Apply PCA to reduce x_data to 3 dimensions
+pca = PCA(n_components=3)
+x_3d = pca.fit_transform(x_data)
+
+# Create a 3D scatter plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+scatter = ax.scatter(x_3d[:, 0], x_3d[:, 1], x_3d[:, 2], c=y_data, cmap='viridis')
+
+# Add labels and legend
+ax.set_xlabel('Principal Component 1')
+ax.set_ylabel('Principal Component 2')
+ax.set_zlabel('Principal Component 3')
+ax.set_title('3D Scatter Plot with PCA')
+
+# Add colorbar for the class labels
+cbar = plt.colorbar(scatter)
+cbar.set_label('Class Label')
+
+# Show the plot
+plt.show()
 
 
