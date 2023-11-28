65fd5f40022081b50db4ced261e9d5ad0e389558
hamreliusfilip <91670593+hamreliusfilip@users.noreply.github.com>
2023-11-28 10:38:03 +0100

    Working SVM

--- SVM.py
+++ SVM.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.decomposition import PCA
 
 loaded_data = np.load('encoded_data.npz')
 
@@ -14,64 +15,15 @@ train_y_1 = loaded_data['train_y_1']
 test_x_1 = loaded_data['test_x_1']
 test_y_1 = loaded_data['test_y_1']
 
-# Check the dimensions of the data
-print(len(test_x_0), len(test_y_0))
-print(test_x_0.shape, test_y_0.shape)
-
-# Slutsatser: 
-
 # X är (4588, 300) där 4588 är antalet dokument och 300 är längden på vektorn som representerar ett dokument 
 # Y är (4588, 1) och representerar integers (0, 1, 2) alltså typen av bias.
 
 # print(train_y_0[0:10]) -> [1,1,1,2,1,0,0,2,0,0]
 
-
-#----------------- Plotting with PCA -----------------#
-
-#----------------- Stäng ner pop up fönstret med PCA för att se resultatet av SVM -----------------#
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.decomposition import PCA
-
-x_data = test_x_0
-y_data = test_y_0
-
-# Apply PCA to reduce x_data to 3 dimensions
-pca = PCA(n_components=3)
-x_3d = pca.fit_transform(x_data)
-
-# Create a 3D scatter plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-scatter = ax.scatter(x_3d[:, 0], x_3d[:, 1], x_3d[:, 2], c=y_data, cmap='viridis')
-
-# Add labels and legend
-ax.set_xlabel('Principal Component 1')
-ax.set_ylabel('Principal Component 2')
-ax.set_zlabel('Principal Component 3')
-ax.set_title('3D Scatter Plot with PCA')
-
-# Add colorbar for the class labels
-cbar = plt.colorbar(scatter)
-cbar.set_label('Class Label')
-
-# Show the plot
-plt.show()
-
-#------------------------------------------------------#
-
-#----------------- Support vector machine -----------------#
-
-# Simple linear support vector machine
-
-import numpy as np
-
 class SVM:
     
-    def __init__(self, learning_rate=0.01, lambda_param=0.01, n_iters=10):
-        # Initialization method for the SVM class
-        # It sets up the hyperparameters and attributes of the SVM model
+    def __init__(self, learning_rate=0.01, lambda_param=0.01, n_iters=10): # Får samma resultat med 1000 & 100
+       
         self.lr = learning_rate  # Learning rate for gradient descent
         self.lambda_param = lambda_param  # Regularization parameter
         self.n_iters = n_iters  # Number of iterations for training
@@ -79,36 +31,31 @@ class SVM:
         self.b = None  # Bias term
 
     def fit(self, X, y):
-        # Training method for the SVM model
-        # X: Feature matrix (training data)
-        # y: Vector of labels
 
+        print("y shape: ", y.shape)
+        
         n_samples, n_features = X.shape  # Get the number of samples and features in the training data
         self.w = np.zeros((len(np.unique(y)), n_features))  # Initialize the weight matrix
         self.b = np.zeros(len(np.unique(y)))  # Initialize the bias vector
 
-        for _ in range(self.n_iters):
-            # Iterating over the specified number of training iterations
+        for class_label in np.unique(y):
+         # Set class labels to -1 for samples not belonging to the current class
+            binary_labels = np.where(y == class_label, 1, -1)
+            for _ in range(self.n_iters):  # Total iterations
+                for idx, x_i in enumerate(X):  # Each sample
+                    # Compute the condition based on the hinge loss
+                    condition = binary_labels[idx] * (np.dot(x_i, self.w[class_label].T) - self.b[class_label]) >= 1
+                    if condition.all():
+                        # If the condition holds for all samples, update weights with regularization term
+                        self.w[class_label] = self.w[class_label] - self.lr * (2 * self.lambda_param * self.w[class_label])
+                    else:
+                        # If the condition is not met, update weights and bias to account for misclassifications
+                        self.w[class_label] = self.w[class_label] - self.lr * (2 * self.lambda_param * self.w[class_label] - np.outer(binary_labels[idx], x_i)) 
+                        self.b[class_label] = self.b[class_label] - self.lr * binary_labels[idx]
 
-            for idx, x_i in enumerate(X):
-                # Iterating over each training sample
-
-                # Compute the condition based on the hinge loss
-                condition = y[idx] * (np.dot(x_i, self.w.T) - self.b) >= 1
-
-                if condition.all():
-                    # If the condition holds for all samples, update weights with regularization term
-                    self.w -= self.lr * (2 * self.lambda_param * self.w)
-                else:
-                    # If the condition is not met, update weights and bias to account for misclassifications
-                    self.w -= self.lr * (2 * self.lambda_param * self.w - np.outer(y[idx], x_i))
-                    self.b -= self.lr * y[idx]
 
     def predict(self, X):
-        # Prediction method for the SVM model
-        # X: Feature matrix for making predictions
-
-        # Return the index of the class with the highest score for each sample
+    # Return the index of the class with the highest score for each sample
         return np.argmax(np.dot(X, self.w.T) - self.b, axis=1)
 
 # Initialize and train the SVM
@@ -122,32 +69,15 @@ predictions = svm_model.predict(test_x_0)
 accuracy = np.mean(predictions == test_y_0)  # Compute accuracy by comparing predictions with true labels
 print(f"Accuracy: {accuracy * 100:.2f}%")  # Print the accuracy of the trained SVM model
 
+# PCA
+pca = PCA(n_components=3)  
+test_x_0_pca = pca.fit_transform(train_x_0)
+train_x_0_pca = pca.transform(test_x_0)
 
-#------------------------- PLOT -----------------------------#
+svm_model = SVM()
+svm_model.fit(train_x_0_pca, train_y_0)
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#scatter = ax.scatter(test_x_0[:, 0], test_x_0[:, 1], test_x_0[:, 2], c=test_y_0, cmap='viridis', marker='o', label='Data Points')
+predictions = svm_model.predict(test_x_0_pca)
 
-# Plot the decision boundary (hyperplane)
-#xlim = ax.get_xlim()
-#ylim = ax.get_ylim()
-#zlim = ax.get_zlim()
-
-#xx, yy, zz = np.meshgrid(np.linspace(xlim[0], xlim[1], 10), np.linspace(ylim[0], ylim[1], 10), np.linspace(zlim[0], zlim[1], 10))
-#xyz = np.column_stack([xx.ravel(), yy.ravel(), zz.ravel()])
-
-#Z = svm_model.predict(xyz)
-#Z = Z.reshape(xx.shape)
-
-#ax.contour3D(xx, yy, Z, colors='k', levels=[-1, 0, 1], alpha=0.5, linestyles=['--', '-', '--'])
-
-# Add labels and legend
-#ax.set_xlabel('Dimension 1')
-#ax.set_ylabel('Dimension 2')
-#ax.set_zlabel('Dimension 3')
-#ax.legend()
-#plt.title('SVM Classification with Decision Boundary')
-
-# Show the plot
-#plt.show()
+accuracy = np.mean(predictions == test_y_0)
+print(f"Accuracy with PCA: {accuracy * 100:.2f}%")
--- SVM_Example.py
+++ SVM_Example.py
@@ -1,53 +1,4 @@
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import random
-import time
-import matplotlib.pyplot as plt
-
-from sklearn.datasets import make_blobs
-
-X, Y = make_blobs(n_samples = 40,centers=2, cluster_std=1.2,n_features=2,random_state=42)
-
-#convert to -1's and 1's
-for i,j in enumerate(Y):
-    if j == 0:
-        Y[i] = -1
-    elif j == 1:
-        Y[i] = 1
-        
-#group for plotting
-df = pd.DataFrame(dict(x=X[:,0], y=X[:,1], label=Y))
-names = {-1:'Group 1', 1:'Group 2'}
-colors = {-1:(0,100/255,0,0.9), 1:(138/255,43/255,226/255,0.9)}
-grouped = df.groupby('label')
-
-#Example Lines: 2 points needed per line
-ex_line_x1 = np.linspace(-4,6,100)
-ex_line_y1 = 1*ex_line_x1+4
-ex_line_x2 = np.linspace(-4,6,100)
-ex_line_y2 = 0.2*ex_line_x2+4.5
-ex_line_x3 = np.linspace(-4,6,100)
-ex_line_y3 = -0.1*ex_line_x3+5.5
-
-#plot settings and labels
-fig = plt.figure(figsize=(13,9))
-ax = fig.add_subplot(1, 1, 1)
-ax.set_title("Two Clusters of Data", fontsize=20)
-ax.set_xlabel("Feature 1", fontsize=18)
-ax.set_ylabel("Feature 2", fontsize=18)
-ax.set_facecolor((245/255,245/255,245/255))
-
-#plot the data and example line
-for key, group in grouped:
-    ax.scatter(group.x,group.y, label=names[key], color=colors[key],edgecolor=(0,0,0,0.75),s=350)
-ax.plot(ex_line_x1, ex_line_y1, color=(0.95,0.1,0.2,0.8), label='Line 1',linewidth=4)
-ax.plot(ex_line_x2, ex_line_y2, color=(0.1,0.3,0.95,0.8), label='Line 2',linewidth=4)
-ax.plot(ex_line_x3, ex_line_y3, color=(0.1,0.7,0.8,0.8), label='Line 3',linewidth=4)
-ax.legend(markerscale=1,fontsize="x-large")
-plt.show()
-
-#prepare datasets
 #test sets
 x_test = X[20:]
 x_test = np.c_[x_test,np.ones(len(x_test))]
@@ -56,6 +7,7 @@ y_test = Y[20:]
 #training sets
 x = X[:20]
 y = Y[:20]
+
 #group for plotting
 df_train = pd.DataFrame(dict(x=x[:,0], y=x[:,1], label=y))
 grouped_train = df_train.groupby('label')
@@ -68,6 +20,7 @@ w = np.zeros(len(x[0]))
 
 #learning rate 
 lam = 0.001
+
 #array of number for shuffling
 order = np.arange(0,len(x),1)
 
@@ -78,7 +31,7 @@ pos_support_vectors = 0
 neg_support_vectors = 0
 
 not_converged = True
-t =0 
+t = 0 
 start_time = time.time()
 
 while(not_converged):
@@ -113,8 +66,6 @@ while(not_converged):
         if((pos_support_vectors > 0)and(neg_support_vectors > 0)and((margin_current - margin_previous) < 0.01)):
             not_converged = False
 
-#print running time
-print("--- %s seconds ---" % (time.time() - start_time))
 
 #create grid to draw decision boundary
 grid_res = 200
@@ -139,17 +90,7 @@ for i in range(grid_res):
 grid = np.asarray(grid)
 gridy = np.asarray(gridy)
 
-#plot the data
-fig = plt.figure(figsize=(15,11))
-ax = fig.add_subplot(1, 1, 1)
-ax.set_title("Blobs Classified by SVM", fontsize=20)
-ax.set_xlabel("Feature 1", fontsize=18)
-ax.set_ylabel("Feature 2", fontsize=18)
-ax.scatter(grid[:,0], grid[:, 1], marker='o',c=gridy,s=10)
-for key, group in grouped_train:
-    ax.scatter(group.x,group.y, label=names[key], color=colors[key],edgecolor=(0,0,0,0.75),s=350)
-ax.legend(markerscale=1,fontsize=20,fancybox=True)
-plt.show()
+
 
 #test classifier on test set
 y_pred = ([])
@@ -168,23 +109,3 @@ for i,val in enumerate(y_pred):
     else:
         y_pred_labels.append(0)
         
-#group for plotting
-df_test = pd.DataFrame(dict(x=x_test[:,0], y=x_test[:,1], pred=y_pred_labels,label=y_test))
-grouped_test = df_test.groupby('label')
-grouped_pred = df_test.groupby('pred') 
-pred_colors = {1:'lime', 0:'red'}
-pred_names = {1:'correct',0:'incorrect'}
-
-#plot decision grid with prediction values
-fig = plt.figure(figsize=(15,11))
-ax = fig.add_subplot(1, 1, 1)
-ax.set_title("Blobs Classified by SVM", fontsize=20)
-ax.set_xlabel("Feature 1", fontsize=18)
-ax.set_ylabel("Feature 2", fontsize=18)
-ax.scatter(grid[:,0], grid[:, 1], marker='o',c=gridy,s=10)
-for key, group in grouped_test:
-    ax.scatter(group.x,group.y, label=names[key], color=colors[key],edgecolor=(0,0,0,0),s=350)
-for key, group in grouped_pred:
-    ax.scatter(group.x,group.y,label=pred_names[key],color=(0,0,0,0),linewidth=2,edgecolor=pred_colors[key],s=350)
-ax.legend(markerscale=1,fontsize=20,fancybox=True)
-plt.show()
